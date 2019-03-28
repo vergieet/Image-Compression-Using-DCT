aecc0093271c73e88152bdced9accf4b78dcea99
Vergie Ericson T <vergieericsont@gmail.com>
2019-03-28 18:11:57 +0700

    moving assets

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+app.use('/assets',express.static(__dirname + '/assets'))
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
